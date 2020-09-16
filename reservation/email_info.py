b4b7a58d0f6bf980339c8fff18ff56e35dfbd74d
Amirmohammad Raei <50745675+amiirmohammad@users.noreply.github.com>
2020-09-16 14:23:34 +0430

    Update email_info.py

--- reservation/email_info.py
+++ reservation/email_info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST ='smtp.gmail.com'
-EMAIL_HOST_USER = 'amir.m.raei@gmail.com'
-EMAIL_HOST_PASSWORD = 'Mohammad79'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = '' 
 EMAIL_PORT = 587
